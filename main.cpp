7b587c1cbc1678b949b4e9ae5a1563a417eeb68e
Miguel Algaba <miguel.algaba.borrego@gmail.com>
2012-11-05 12:26:55 +0100

    Bumped version to 1.0

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include "oneFeature.h"
 
 int main()
 {
     std::cout << "main()" << std::endl;
+    std::cout << "pow(2.0,10.0): " << oneFeature<double>(2.0,10.0) << std::endl;
 
     return 0;
 }
