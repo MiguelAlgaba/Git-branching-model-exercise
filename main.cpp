e203b8e86976bf9336138574a364197958b8dfc9
Miguel Algaba <miguel.algaba.borrego@gmail.com>
2012-11-06 09:57:31 +0100

    Added a dummy test to try CTest

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 #include "oneFeature.h"
 #include "anotherFeature.h"
 
@@ -7,6 +8,13 @@ int main()
     std::cout << "main()" << std::endl;
     std::cout << "pow(2.0,10.0): " << oneFeature<double>(2.0,10.0) << std::endl;
     std::cout << "sqrt(144): " << anotherFeature<unsigned int>(144) << std::endl;
+
+    bool some_failure = true;
+
+    if (some_failure)
+         return EXIT_FAILURE;
+    else
+         return EXIT_SUCCESS;
 
     return 0;
 }
