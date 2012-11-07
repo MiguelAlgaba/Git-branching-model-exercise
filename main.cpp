f409a381d99165db39acfa370261c557de39012a
Miguel Algaba <miguel.algaba.borrego@gmail.com>
2012-11-07 12:51:12 +0100

    Added one warning to try the 'Warnings Plug-in' for Jenkins

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 
 int main()
 {
+    int this_is_an_unused_variable; //should produce a warning
+
     std::cout << "main()" << std::endl;
     std::cout << "pow(2.0,10.0): " << oneFeature<double>(2.0,10.0) << std::endl;
     std::cout << "sqrt(144): " << anotherFeature<unsigned int>(144) << std::endl;
