01860750aa71eeaeae73dbb05d9a9b048de9f720
Miguel Algaba <miguel.algaba.borrego@gmail.com>
2012-11-07 16:34:48 +0100

    Added some comments to the expected warnings

--- main.cpp
+++ main.cpp
@@ -5,15 +5,14 @@
 
 int main()
 {
-    int this_is_an_unused_variable; //should produce a warning
-    int this_is_an_int_variable = 13; //should produce a warning
-    float this_is_a_float_variable = 0; //should produce a warning
-    this_is_a_float_variable = this_is_an_int_variable; //should produce a warning
+    int this_is_an_unused_variable; //should produce a warning (unused variable)
+    int this_is_an_int_variable = 13;
+    float this_is_a_float_variable = 0; //should produce two warnings (unused variable / 'int' to 'float' conversion)
+    this_is_a_float_variable = this_is_an_int_variable; //should produce a warning (int to float conversion)
 
     std::cout << "main()" << std::endl;
     std::cout << "pow(2.0,10.0): " << oneFeature<double>(2.0,10.0) << std::endl;
-    std::cout << "sqrt(144): " << anotherFeature<unsigned int>(144) << std::endl;
-
+    std::cout << "sqrt(144): " << anotherFeature<unsigned int>(144) << std::endl; //should produce a warning (instantiation of sqrt function with 'unsigned int values'. Produces an 'unsigned int' from 'double' conversion warning)
     bool some_failure = false;
 
     if (some_failure)
